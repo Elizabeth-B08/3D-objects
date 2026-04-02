b29fc315a6942f61f41269a787c81c2bc269af34
elizabethb90 <elizabethb90@kprschools.ca>
2026-04-02 13:32:28 -0400

    hhi

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 from shape3d import Shape3D
 
 def main():
-    shape = Shape3D("Generic Shape", "Red")
+    shape = Shape3D("Generic Shape", "Red", 1, 1, 1)
 
-    # Test getters
     print("Type:", shape.get_type())
     print("Colour:", shape.get_colour())
 
-    # Test setters
     shape.set_type("Updated Shape")
     shape.set_colour("Blue")
 
     print("Updated Type:", shape.get_type())
     print("Updated Colour:", shape.get_colour())
 
-    # Test methods
     print("Volume:", shape.volume())
     print("Surface Area:", shape.surface_area())
 
